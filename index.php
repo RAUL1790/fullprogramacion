a8d9fccf84a5cb46e97962a6a707316d3e1809f4
Raul <amos.1790@yahoo.com>
2014-12-08 22:23:29 -0430

    actualizacion

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>fullprogramación</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
