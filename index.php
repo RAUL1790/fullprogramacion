3964283076f79d445f0e8e00ea5a066deab58189
Raul <amos.1790@yahoo.com>
2014-12-09 22:39:22 -0430

    archivos

--- index.php
+++ index.php
@@ -6,11 +6,61 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>fullprogramación</title>
 	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
 </head>
 <body>
+<div class="wrapper">
+<div class="row">
+ etiqueta
+</div>
+<div class="container-fluid" id="contenedor">
+ <div class="row">
+ 	<div class="col-md-7" id="izquierda" >
 
 
 
+
+
+ 	</div>
+    <div class="col-md-5">
+		    <div role="tabpanel" id="panel">
+
+					  <!-- Nav tabs -->
+					  <ul class="nav nav-tabs" role="tablist">
+					    <li role="presentation" class="active"><a href="#" id="lg" aria-controls="login" role="tab" data-toggle="tab">Login</a></li>
+					    <li role="presentation"><a href="#"  id="rg" aria-controls="registro" role="tab" data-toggle="tab">Registro</a></li>
+					  
+					  </ul>
+
+					  <!-- Tab panes -->
+					  <div class="tab-content">
+					    <div role="tabpanel" class="tab-pane active" id="login"></div>
+					    <div role="tabpanel" class="tab-pane" id="registro"></div>
+					  </div>
+
+		</div>
+      <div class="login">
+         <h3>Login</h3>
+          <input type="text" autofocus placeholder="Correo Electrónico" id="correo" name="correo" maxlength="40"><br>
+          <input type="text" autofocus placeholder="Contraseña"  id="password" name="password" maxlength="40"><br>
+          <button class="btn btn-success btn-lg" id="btnLogin" type="button">Login</button>
+
+      </div>
+      <div class="registro"  style="display:none;" >
+         <h3>Registro</h3>
+          <input type="text" autofocus placeholder="Nombre Completo" id="nombre" name="nombre" maxlength="40"><br>
+          <input type="text" autofocus placeholder="Correo Electrónico" id="correo" name="correo" maxlength="40"><br>
+          <input type="text" autofocus placeholder="Contraseña"  id="password" name="password" maxlength="40"><br>
+          
+          <button class="btn btn-success btn-lg" id="btnRegistro" type="button">Regístrarse</button>
+      </div>
+
+    </div>
+ </div>
+ 
+</div>
+
+</div>
 
 
 
@@ -24,5 +74,6 @@
 
 <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/funciones.js"></script>
 </body>
 </html>
