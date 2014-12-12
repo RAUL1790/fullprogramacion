b7e6975b4252a22ab328d389d50b591d876dba0d
Raul <amos.1790@yahoo.com>
2014-12-11 22:12:14 -0430

    cambios finales

--- index.php
+++ index.php
@@ -14,62 +14,50 @@
  etiqueta
 </div>
 <div class="container-fluid" id="contenedor">
- <div class="row">
- 	<div class="col-md-7" id="izquierda" >
+   <div class="row">
+     	<div class="col-md-7" >
 
 
 
 
 
- 	</div>
-    <div class="col-md-5">
-		    <div role="tabpanel" id="panel">
+     	</div>
+              <div class="col-md-5">
 
-					  <!-- Nav tabs -->
-					  <ul class="nav nav-tabs" role="tablist">
-					    <li role="presentation" class="active"><a href="#" id="lg" aria-controls="login" role="tab" data-toggle="tab">Login</a></li>
-					    <li role="presentation"><a href="#"  id="rg" aria-controls="registro" role="tab" data-toggle="tab">Registro</a></li>
-					  
-					  </ul>
+                    <div role="tabpanel" id="panel">
 
-					  <!-- Tab panes -->
-					  <div class="tab-content">
-					    <div role="tabpanel" class="tab-pane active" id="login"></div>
-					    <div role="tabpanel" class="tab-pane" id="registro"></div>
-					  </div>
-
-		</div>
-      <div class="login">
-         <h3>Login</h3>
-          <input type="text" autofocus placeholder="Correo Electrónico" id="correo" name="correo" maxlength="40"><br>
-          <input type="text" autofocus placeholder="Contraseña"  id="password" name="password" maxlength="40"><br>
-          <button class="btn btn-success btn-lg" id="btnLogin" type="button">Login</button>
-
-      </div>
-      <div class="registro"  style="display:none;" >
-         <h3>Registro</h3>
-          <input type="text" autofocus placeholder="Nombre Completo" id="nombre" name="nombre" maxlength="40"><br>
-          <input type="text" autofocus placeholder="Correo Electrónico" id="correo" name="correo" maxlength="40"><br>
-          <input type="text" autofocus placeholder="Contraseña"  id="password" name="password" maxlength="40"><br>
-          
-          <button class="btn btn-success btn-lg" id="btnRegistro" type="button">Regístrarse</button>
-      </div>
-
-    </div>
- </div>
- 
-</div>
-
-</div>
+                      <!-- Nav tabs -->
+                      <ul class="nav nav-tabs" role="tablist">
+                        <li role="presentation" class="active"><a href="#Login" aria-controls="Login" role="tab" data-toggle="tab">Login</a></li>
+                        <li role="presentation"><a href="#Registro" aria-controls="Registro" role="tab" data-toggle="tab">Registro</a></li>
+                      </ul>        
+                  <div class="tab-content">
+                    <div role="tabpanel" class="tab-pane active" id="Login">
+                             <h3>Login</h3>
+                            <input type="text" autofocus placeholder="Correo Electrónico" id="correo" name="correo" maxlength="40"><br>
+                            <input type="text" autofocus placeholder="Contraseña"  id="password" name="password" maxlength="40"><br>
+                            <button class="btn btn-success btn-lg" id="btnLogin" type="button">Login</button>
+                    </div>
+                    <div role="tabpanel" class="tab-pane" id="Registro">
+                      <form method="post" id="reg_usu" name="reg_usu" onsubmit="return valRegistro()" action="js/index/registro.php">
+                       <h3>Registro</h3>
+                          <div style="text-align:center;color:#fff;" id="errores" ></div>
+                          <input type="text" autofocus placeholder="Nombre Completo" id="nombre" name="nombre" maxlength="40"><br>
+                          <input type="text" autofocus placeholder="Correo Electrónico" id="correo" name="correo" maxlength="40"><br>
+                          <input type="text" autofocus placeholder="Contraseña"  id="password" name="password" maxlength="40"><br>
+                          
+                          <input class="btn btn-success btn-lg" id="btnRegistro" type="submit" value="Regístrarse">
+                      </form>
+                    </div>
+                  </div>
+                 </div>	
+              </div> <!-- fin col-md-5 -->
+     </div><!-- row -->
+   
+</div><!-- contenedor -->
 
 
-
-
-
-
-
-
-
+</div><!-- wrapper -->
 
 
 <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
--- js/funciones.js
+++ js/funciones.js
@@ -1,14 +1,34 @@
 $(document).ready(function(){
-
-          $('#lg').click(function(event) {
-          	$(".registro").hide();
-            $(".login").show();
-          });
-         
-           $('#rg').click(function(event) {
-          	$(".login").hide();
-            $(".registro").show();
-          });
+	
+$('#reg_usu')[0].reset();
 
 
 });
+
+    function valCorreo( c ){
+     var regex = /[\w-\.]{2,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/;
+    if (regex.test(c)) { return true; } else { return false; }
+    }
+	function valRegistro(){
+		var n = $('#reg_usu #nombre').val();
+		var c = $('#reg_usu #correo').val();
+		var p = $('#reg_usu #password').val();
+		if(n.length < 4){
+          $("#errores").html("Usuario minimo : 4 caracteres");
+          return false;
+		}else{  $("#errores").html('');}
+            
+
+        if(valCorreo(c)==false){
+             $("#errores").html("Formato de correo no valida");
+             return false;
+        }else{ $("#errores").html('');}
+ 
+        if(p.length < 4){
+          $("#errores").html("Contraseña minima : 4 caracteres");
+          return false;
+		}else{  $("#errores").html('');}
+
+
+     return true;
+	}
