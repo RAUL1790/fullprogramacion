e0f85b99282f2763e403ac86ade55492798d40b1
Raul <amos.1790@yahoo.com>
2014-12-17 22:59:54 -0430

    navegacion

---
+++ temas.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-inverse" role="navigation">
+  <div class="container-fluid">
+ 
+
+    <div class="navbar-header">
+      <a class="navbar-brand" href="principal.php">Temas</a><br>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li ><a href="tutoriales.php">Html</a></li><br>
+        <li ><a href="acerca.php">PHP</a></li><br>
+        <li ><a href="acerca.php">Jquery</a></li><br>
+        <li ><a href="acerca.php">Phyton</a></li><br>
+        <li ><a href="acerca.php">MySql</a></li><br>
+        <li ><a href="acerca.php">Libros</a></li><br>   
+        <li class="divider"></li>     
+      </ul>     
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
